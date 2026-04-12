c1fbd21ec3715ddd884f1e9b38a376f3aafea7c7
Dumindu A Liyanaarachchi <dumindu.akhila2003@gmail.com>
2026-04-12 11:30:00 +0530

    add the utillts to the paymentSimulator

--- letgo-driver-module/utils/paymentSimulator.js
+++ letgo-driver-module/utils/paymentSimulator.js
@@ -8,5 +8,5 @@ const processPayment = async (amount) => {
     }, 1000);
   });
 };
-
+// utils/paymentSimulator.js - Simulates payment processing for card and cash payments. Used in demo routes to mimic real payment flows without actual integration.
 module.exports = { processPayment };
